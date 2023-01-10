6acbc58527579c4211d7b9a09e0279e8c4bd2382
сулпак <sm.mederovich@gmail.com>
2023-01-11 00:13:23 +0600

    first commit

---
+++ hw/js_2_dz/main.js
@@ -0,0 +1,34 @@
+var jojo = {
+    name: 'Сериал: JoJo’s Bizarre Adventure',
+    // JoJoS_Bizarre_Adventure: '190 episodes.',
+    // surname: 'M4',
+    releaseDate: ' Был выпущен 5 July 2012 y',
+    mainActor: 'В главных ролях: Джонатан Джостар, Джозеф Джостар, Джотаро Куджо, Джоске Хигашиката, Джорно Джованна, Джолин Куджо, Джонни Джостар, Джоске Хигашиката',
+    episodes: 'В JoJo’s Bizarre Adventure',
+    episodes:{
+        Phantom_Blood: 'Phantom Blood: 9 серий',
+        Battle_Tendency: 'Battle Tendency: 17 серий',
+
+        Stardust_Crusaders: 'Stardust Crusaders: 48 серий',
+        Diamond_Is_Unbreakable: 'Diamond Is Unbreakable: 39 серий',
+
+        Golden_Wind: 'Golden Wind: 39 серий',
+        Stone_Ocean: 'Stone Ocean: 38 серий',
+    },
+    dio:'Очень Советую Посмотреть'
+}
+alert(jojo.name)
+
+alert(jojo.releaseDate)
+alert(jojo.mainActor)
+
+alert(jojo.episodes.Phantom_Blood)
+alert(jojo.episodes.Battle_Tendency)
+
+alert(jojo.episodes.Stardust_Crusaders)
+alert(jojo.episodes.Diamond_Is_Unbreakable)
+
+alert(jojo.episodes.Golden_Wind)
+alert(jojo.episodes.Stone_Ocean)
+
+alert(jojo.dio)
---
+++ hw/js_2_dz/scnd_task.js
@@ -0,0 +1,10 @@
+var day = 'Monday';
+
+switch (day) {
+  case 'Monday':
+    alert( 'Понедельник' );
+    break;
+  case 'monday':
+    alert( 'понедельник' );
+    break;
+}
